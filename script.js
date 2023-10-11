8696d514be478fa4667b6e1b6da5ab3d84168c42
dani009988 <142094407+dani009988@users.noreply.github.com>
2023-10-11 02:07:20 -0700

    ..

---
+++ script.js
@@ -0,0 +1,7 @@
+const progressContent = document.querySelector(".autoplay-progress span");
+
+const swiperEl = document.querySelector("swiper-container");
+swiperEl.addEventListener("autoplaytimeleft", (e) => {
+  const [swiper, time, progress] = e.detail;
+  progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+});
